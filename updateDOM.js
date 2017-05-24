5889cede7927f7381ab02f4eeecbb7ee4ef5c52e
mer <mersaythis@gmail.com>
2017-05-24 10:04:18 -0400

    heroku node fix

--- updateDOM.js
+++ updateDOM.js
@@ -3,7 +3,7 @@
  */
 
 $(document).ready(function() {
-  $.getJSON('http://localhost:3000/getTopAlbums', function(data) {
+  $.getJSON('https://floating-falls-55555.herokuapp.com/getTopAlbums', function(data) {
     let topAlbums = data[0].topalbums.album;
     Object.keys(topAlbums).forEach((key) => {
       let link = topAlbums[key].image[3]["#text"];  //3 for xlg img
@@ -12,7 +12,7 @@ $(document).ready(function() {
     );
   });
 
-  $.getJSON('http://localhost:3000/getRecentTracks', function(data) {
+  $.getJSON('https://floating-falls-55555.herokuapp.com/getRecentTracks', function(data) {
     let recentTrackName = data[0].recenttracks.track[0].name;
     let recentTrackArtist = data[0].recenttracks.track[0].artist['#text'];
     console.log(recentTrackName,recentTrackArtist);
